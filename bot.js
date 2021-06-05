f765e6a57a7b8d28dec0c6c5a0e005ae91677735
Waisu <85125438+Waisu242@users.noreply.github.com>
2021-06-05 23:36:53 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js-selfbot");
+
+let tokens = ["TOKEN", "TOKEN"]; // İstediğin Kadar Arttırabilirsin!
+
+for (let index = 0; index < tokens.length; index++) {
+
+const client = new Discord.Client();
+client.login(tokens[index]).catch(err => console.error(err));
+
+client.on('ready', () => {
+console.log(`${client.user.username} İsimli Token Aktif Oldu!`);
+});
+
+};
