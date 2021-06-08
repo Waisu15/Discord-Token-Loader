58d23cdc7ce7f000bda2aa64516ab7c63a6cb2e8
Waisu <85125438+Waisu242@users.noreply.github.com>
2021-06-08 18:46:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js-selfbot");
 
 let tokens = ["TOKEN", "TOKEN"]; // İstediğin Kadar Arttırabilirsin!
 
-for (let index = 0; index < tokens.length; index++) {
+for(let index = 0; index < tokens.length; index++) {
 
 const client = new Discord.Client();
 client.login(tokens[index]).catch(err => console.error(err));
